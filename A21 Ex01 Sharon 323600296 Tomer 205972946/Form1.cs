f70bdc199688e8d8f0a9f4eb486b62cf1fd3fc71
mega5800 <43865655+mega5800@users.noreply.github.com>
2020-11-07 17:20:42 +0200

    Add project files.

---
+++ A21 Ex01 Sharon 323600296 Tomer 205972946/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace A21_Ex01_Sharon_323600296_Tomer_205972946
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
