0cf94aaff79eafae0b86fa1fc1b58e826e347f99
mega5800 <43865655+mega5800@users.noreply.github.com>
2020-11-07 17:22:53 +0200

    first commit

--- A21 Ex01 Sharon 323600296 Tomer 205972946/Form1.cs
+++ A21 Ex01 Sharon 323600296 Tomer 205972946/Form1.cs
@@ -11,9 +11,15 @@ namespace A21_Ex01_Sharon_323600296_Tomer_205972946
 {
     public partial class Form1 : Form
     {
+        // comment
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
