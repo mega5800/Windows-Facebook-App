20634a0a77d0b9d9e3acf96d4bd53978a9948904
Tomer Priel <tomerprielg@gmail.com>
2020-11-18 18:15:11 +0200

    added likedPages button functionality

---
+++ A21 Ex01 Sharon 323600296 Tomer 205972946/Forms/LikedPagesForm.cs
@@ -0,0 +1,37 @@
+﻿using FacebookWrapper.ObjectModel;
+using System;
+using System.Windows.Forms;
+
+namespace Ex01.FacebookAppUI.Forms
+{
+    public partial class LikedPagesForm : Form
+    {
+        private User m_LoggedInUser;
+
+        public LikedPagesForm(User i_LoggedInUser)
+        {
+            InitializeComponent();
+            this.m_LoggedInUser = i_LoggedInUser;
+        }
+
+        // PRIVATE METHODS
+        private void fetchUserLikedPages()
+        {
+            foreach (Page fbPage in this.m_LoggedInUser.LikedPages)
+            {
+                this.listBoxUserLikedPages.Items.Add(string.Format("Name: {0} | Description: {1}", fbPage.Name, fbPage.Description));
+            }
+
+            if (this.m_LoggedInUser.LikedPages.Count == 0)
+            {
+                MessageBox.Show("The user has not liked any pages!");
+            }
+        }
+
+        // EVENTS
+        private void LikedPagesForm_Load(object sender, EventArgs e)
+        {
+            fetchUserLikedPages();
+        }
+    }
+}
