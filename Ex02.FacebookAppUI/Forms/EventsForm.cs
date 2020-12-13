bf9ac4a66574fe805b055aad195f73adf30ebc39
mega5800 <43865655+mega5800@users.noreply.github.com>
2020-12-14 00:08:02 +0200

    added events data binding

--- Ex02.FacebookAppUI/Forms/EventsForm.cs
+++ Ex02.FacebookAppUI/Forms/EventsForm.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Threading;
+﻿using System.Threading;
 using System.Windows.Forms;
 using Ex02.FacebookAppLogic.Classes;
-using Ex02.FacebookAppUI.Enums;
-using Ex02.FacebookAppUI.Loaders;
 using FacebookWrapper.ObjectModel;
 
 namespace Ex02.FacebookAppUI.Forms
@@ -12,31 +9,20 @@ namespace Ex02.FacebookAppUI.Forms
     {
         // ATTRIBUTES
         private readonly Thread r_StartThread;
-        private readonly List<object> r_ParamsList;
         private User m_LoggedInUser;
-        private Loader<Event> m_TextLoader;
         
         // CTOR
         public EventsForm()
         {
             InitializeComponent();
             this.m_LoggedInUser = LoggedInUser.Instance;
-            this.r_ParamsList = new List<object>() { this.m_LoggedInUser.Events, this.listBoxUserEvents, "{0} | Time range: {1} - {2}" };
             this.r_StartThread = new Thread(new ThreadStart(loadForm));
-            this.m_TextLoader = LoaderFactory<Event>.CreateLoader(eLoaderFactoryContext.CreateTextLoader, this.r_ParamsList);
         }
 
         // PRIVATE METHODS
         private void loadForm()
         {
-            if (this.m_LoggedInUser.Events.Count != 0)
-            {
-                this.m_TextLoader.LoadProperties("Name", "StartTime", "EndTime");
-            }
-            else
-            {
-                MessageBox.Show(string.Format("{0}, you do not have any events!", this.m_LoggedInUser.FirstName));
-            }
+            eventBindingSource.DataSource = this.m_LoggedInUser.Events;
         }
 
         // EVENTS
--- Ex02.FacebookAppUI/Forms/PostsForm.cs
+++ Ex02.FacebookAppUI/Forms/PostsForm.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Windows.Forms;
 using Ex02.FacebookAppLogic.Classes;
-using Ex02.FacebookAppUI.Enums;
-using Ex02.FacebookAppUI.Loaders;
 using FacebookWrapper.ObjectModel;
 
 namespace Ex02.FacebookAppUI.Forms
@@ -13,31 +10,19 @@ namespace Ex02.FacebookAppUI.Forms
     {
         // ATTRIBUTES
         private readonly Thread r_StartThread;
-        private readonly List<object> r_ParamsList;
         private User m_LoggedInUser;
-        private Loader<Post> m_TextLoader;
 
         // CTOR
         public PostsForm()
         {
             InitializeComponent();
             this.m_LoggedInUser = LoggedInUser.Instance;
-            this.r_ParamsList = new List<object>() { this.m_LoggedInUser.Posts, this.listBoxUserPosts, "{0}" };
             this.r_StartThread = new Thread(new ThreadStart(loadForm));
-            this.m_TextLoader = LoaderFactory<Post>.CreateLoader(eLoaderFactoryContext.CreateTextLoader, this.r_ParamsList);
         }
 
         // PRIVATE METHODS
         private void loadForm()
         {
-            /*if (this.m_LoggedInUser.Posts.Count != 0)
-            {
-                this.m_TextLoader.LoadProperties("Message");
-            }
-            else
-            {
-                MessageBox.Show(string.Format("{0}, you do not have any posts!", this.m_LoggedInUser.FirstName));
-            }*/
             postBindingSource.DataSource = this.m_LoggedInUser.Posts;
         }
 
